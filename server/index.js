8a2b2920431ec2ab24bec9e7b6b1d9b5c600def0
kanno-y <kanno0521@gmail.com>
2023-02-07 00:10:23 +0900

    :pencil2: Add:  "/"へのルーティング設定

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
-const express = require("express");
-const app = express();
-const PORT = 6000;
+const express = require("express"); // expressモジュールを読み込む
+const app = express(); // expressモジュールをインスタンス化してapp変数に代入
+const PORT = 5000;
 
+// HTTP GET 要求を指定されたパス("/")にルーティング
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// サーバーを始動して、ポート 6000 で接続をlistenする
 app.listen(PORT, () => {
   console.log("ローカルサーバー起動中・・・");
 });
