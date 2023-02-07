6fc7a0391f01d9008699aa92531c418c8eece7a5
kanno-y <kanno0521@gmail.com>
2023-02-07 23:01:31 +0900

    :pencil2: Add: DB接続

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 const express = require("express"); // expressモジュールを読み込む
+const mongoose = require("mongoose");
 const app = express(); // expressモジュールをインスタンス化してapp変数に代入
 const PORT = 5000;
+require("dotenv").config;
+
+// DB接続
+try {
+  mongoose.connect(process.env).MONGODB_URL;
+  console.log("DBと接続中・・・");
+} catch (error) {
+  console.log(error);
+}
 
 // HTTP GET 要求を指定されたパス("/")にルーティング
 app.get("/", (req, res) => {
---
+++ server/src/v1/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// モデルの作成
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
