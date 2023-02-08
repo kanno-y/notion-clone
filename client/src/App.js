1aecda908dc7c99e26be1b0ccb724fead80299dc
kanno-y <kanno0521@gmail.com>
2023-02-09 08:46:35 +0900

    :pencil2: Add: 認証用のコンポーネントを作成

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/layout/AuthLayout";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
