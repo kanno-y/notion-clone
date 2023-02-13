c88d3979c3bae9b03a3ea7bf3b82290a20b1649d
kanno-y <kanno0521@gmail.com>
2023-02-14 08:22:05 +0900

    :pencil2: Add: ロゴ追加

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Container } from "@mui/system";
+import { Box } from "@mui/material";
+import notionLogo from "../../assets/images/notion-logo.png";
 
-const AuthLayout = () => {
-  return (
-    <div>
-      AuthLayout <Outlet />
-    </div>
-  );
-};
+const AuthLayout = () => (
+  <div>
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src={notionLogo}
+          alt=""
+          style={{ width: 100, height: 100, marginBottom: 3 }}
+        />
+        Notionクローン
+      </Box>
+    </Container>
+    <Outlet />
+  </div>
+);
 
 export default AuthLayout;
