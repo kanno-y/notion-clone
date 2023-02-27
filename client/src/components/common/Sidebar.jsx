de9d2a573f64295707769772663574d1ceb95d3f
kanno-y <kanno0521@gmail.com>
2023-02-28 00:08:27 +0900

    :up: Update: ログアウト機能を追加

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -8,8 +8,18 @@ import {
 } from "@mui/material";
 import React from "react";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import assets from "../../assets/index.";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Drawer
       container={window.document.body}
@@ -17,7 +27,13 @@ const Sidebar = () => {
       open={true}
       sx={{ width: 250, height: "100vh" }}
     >
-      <List sx={{ width: 250, height: "100vh" }}>
+      <List
+        sx={{
+          width: 250,
+          height: "100vh",
+          backgroundColor: assets.colors.secondary,
+        }}
+      >
         <ListItemButton>
           <Box
             sx={{
@@ -28,10 +44,43 @@ const Sidebar = () => {
             }}
           >
             <Typography variant="body2" fontWeight="700">
-              タイポ
+              ログアウト
+            </Typography>
+            <IconButton aria-label="" onClick={logout}>
+              <LogoutOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              お気に入り
+            </Typography>
+          </Box>
+        </ListItemButton>
+        <Box sx={{ paddingTop: "10px" }}></Box>
+        <ListItemButton>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Typography variant="body2" fontWeight="700">
+              プライベート
             </Typography>
             <IconButton aria-label="" onClick={() => {}}>
-              <LogoutOutlinedIcon />
+              <AddBoxOutlinedIcon fontSize="small" />
             </IconButton>
           </Box>
         </ListItemButton>
