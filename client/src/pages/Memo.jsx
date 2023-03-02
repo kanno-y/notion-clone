ead554d0ced8a117b17fe5160fae51e39f429834
kanno-y <kanno0521@gmail.com>
2023-03-03 08:13:22 +0900

    :up: Update memoコンポーネントのテキストフォームを修正

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { Box } from "@mui/system";
+import { IconButton, TextField } from "@mui/material";
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 
 const Memo = () => {
-  return <div>Memo</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton variant="outlined" color="error">
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: "10px 50px" }}>
+        <TextField
+          placeholder="無題"
+          variant="outlined"
+          fullWidth
+          sx={{
+            ".MuiOutlinedInput-input": { padding: 0 },
+            ".MuiOutlinedInput-notchedOutline": { border: "none" },
+            ".MuiOutlinedInput-root": { fontSize: "2rem", fontWeight: "700" },
+          }}
+        />
+        <TextField
+          placeholder="追加"
+          variant="outlined"
+          fullWidth
+          sx={{
+            ".MuiOutlinedInput-input": { padding: 0 },
+            ".MuiOutlinedInput-notchedOutline": { border: "none" },
+            ".MuiOutlinedInput-root": { fontSize: "1rem" },
+          }}
+        />
+      </Box>
+    </>
+  );
 };
 
 export default Memo;
